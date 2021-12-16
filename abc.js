b3fff79729c7ee2f4d3a65cf16734c304952e32f
TuyenQuang <luongtuyenquang77@gmail.com>
2021-12-16 20:55:26 +0700

    DOM Event - contextmenu - Khi click chuot phai thi lam gi do

--- abc.js
+++ abc.js
@@ -1,44 +1,13 @@
 
-// -------------------- undefined --------------------------------
-// Array
-const info1 = ['Quang', 23]
+const box = document.querySelector('.example')
+const span = document.querySelector('span')
 
-console.log(info1[3]) //undefined
+box.addEventListener('contextmenu', (e) => {
+    e.preventDefault()
+    span.style.display = 'block'
+})
 
-// Object
-const info2 = {
-    name: 'Quang',
-    age: 23
-}
-
-console.log(info2.address); //undefined
-
-// Function
-const doSomething = (name) => {
-    console.log(name);
-}
-
-doSomething() //undefined
-
-// -------------------- null --------------------------------
-let user = {
-    name: 'Nguyen Van A',
-    age: 22
-}
-
-user = null
-
-console.log(user);
-
-// -------------------- So sánh == và === --------------------------------
-// So sánh ==
-const a = null
-const b = undefined
-
-console.log(a == b); // true
-
-// So sánh ===
-const c = null
-const d = undefined
-
-console.log(c === d); // false
+span.addEventListener('click', () => {
+    span.style.display = 'none'
+    console.log('Deleted Successfully');
+})
